af80a04da820142afc247392180ef8e06837be07
Nicolas Aguirre Yacup <nicolas.aguirre.y@gmail.com>
2024-12-03 00:32:58 -0500

    topbar and footer added

---
+++ src/pages/home/home.js
@@ -0,0 +1,8 @@
+import { BasicLayout } from "@/layouts";
+export default function HomePage() {
+  return (
+    <BasicLayout>
+      <h1>Estamos en la Home</h1>
+    </BasicLayout>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,29 @@
-import { React, router } from "react";
-import { Button } from "semantic-ui-react";
-import { useAuth } from "@/hooks";
-import Link from "next/link";
+export { default } from "./home";
+// import { React, router } from "react";
+// import { Button } from "semantic-ui-react";
+// import { useAuth } from "@/hooks";
+// import Link from "next/link";
 
-export default function Index() {
-  const { user, logout } = useAuth();
+// export default function Index() {
+//   const { user, logout } = useAuth();
 
-  console.log("user", useAuth);
-  return (
-    <div>
-      <h2>Games Shop</h2>
+//   console.log("user", useAuth);
+//   return (
+//     <div>
+//       <h2>Games Shop</h2>
 
-      {user ? (
-        <div>
-          <p>
-            Hola, {user.firtsname} {user.lastname}
-          </p>
-          <Button onClick={logout}>Cerrar sesión</Button>
-        </div>
-      ) : (
-        <Link href="/join/sign-in">
-          <Button primary>Ir al login</Button>
-        </Link>
-      )}
-    </div>
-  );
-}
+//       {user ? (
+//         <div>
+//           <p>
+//             Hola, {user.firtsname} {user.lastname}
+//           </p>
+//           <Button onClick={logout}>Cerrar sesión</Button>
+//         </div>
+//       ) : (
+//         <Link href="/join/sign-in">
+//           <Button primary>Ir al login</Button>
+//         </Link>
+//       )}
+//     </div>
+//   );
+// }
