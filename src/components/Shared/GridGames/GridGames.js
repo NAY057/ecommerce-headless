17f4652c285bd6f12c9393c7385a6c183c779bb0
Nicolas Aguirre Yacup <nicolas.aguirre.y@gmail.com>
2025-05-16 00:04:34 -0500

    grid games completed

--- src/components/Shared/GridGames/GridGames.js
+++ src/components/Shared/GridGames/GridGames.js
@@ -1,10 +1,34 @@
 import styles from "./GridGames.module.scss";
+import Link from "next/link";
+import { map } from "lodash";
+import { Label } from "@/components/Shared";
+import { fn } from "@/utils";
 
 export function GridGames(props) {
 	const { games } = props;
+	console.log(games, "yeeeahhh");
+
 	return (
-		<div>
-			<h2>GridGames</h2>
+		<div className={styles.gridGames}>
+			{map(games, (game) => (
+				<Link key={game.id} href={game.slug} className={styles.game}>
+					<div>
+						<img src={game.cover.url} />
+						{game.discount > 0 && (
+							<Label.Discount className={styles.discount}>
+								{`-${game.discount}%`}
+							</Label.Discount>
+						)}
+					</div>
+
+					<div>
+						<span>{game.title}</span>
+						<span className={styles.price}>
+							${fn.calcDiscountedPrice(game.price, game.discount)}
+						</span>
+					</div>
+				</Link>
+			))}
 		</div>
 	);
 }
