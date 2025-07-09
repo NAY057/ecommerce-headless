20ab8db15d928716b8b7f4746be2907ead833631
Nicolas Aguirre Yacup <nicolas.aguirre.y@gmail.com>
2025-07-08 21:54:35 -0500

    juegos y paginacion

--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -1,14 +1,40 @@
 import { useEffect } from "react";
 import { BasicLayout } from "@/layouts";
+import { Container } from "semantic-ui-react";
+import { size } from "lodash";
+import {
+	GridGames,
+	NoResult,
+	Pagination,
+	Separator,
+} from "@/components/Shared";
 
-export default function SearchPage() {
+export default function SearchPage(props) {
+	const { games, pagination, searchText } = props;
+	const hasResult = size(games) > 0;
+
 	useEffect(() => {
 		document.getElementById("search-games").focus();
 	}, []);
 	return (
 		<>
 			<BasicLayout relative isOpenSearch>
-				<h2>Estamos en la busqueda</h2>
+				<Container>
+					<Separator height={50} />
+					{hasResult ? (
+						<>
+							<GridGames games={games} />
+							<Separator height={30} />
+							<Pagination
+								currentPage={pagination.page}
+								totalPage={pagination.pageCount}
+							/>
+						</>
+					) : (
+						<NoResult text="No se han encontrado resultados" />
+					)}
+					<Separator height={100} />
+				</Container>
 			</BasicLayout>
 		</>
 	);
